f1189d358e63c66619200124150645b491c6ef9f
Mohit Sharma <mohits9168@gmail.com>
2024-11-06 01:30:57 +0530

    Added Event Info

--- src/data/EventData.jsx
+++ src/data/EventData.jsx
@@ -1,10 +1,12 @@
 
-import broadcast from "../assets/Events_img/Broadcast.jpg"
 import img2 from "../assets/Events_img/AI Chronicles.avif"
 import img3 from "../assets/Events_img/Advertisement.webp"
+import broadcast from "../assets/Events_img/Broadcast.jpg"
+import img7 from "../assets/Events_img/Clash of Campus.jpg"
+import img6 from "../assets/Events_img/Cryptic Hunt.jpg"
+import img8 from "../assets/Events_img/Designathon.jpg"
 import img4 from "../assets/Events_img/Esports.webp"
 import img5 from "../assets/Events_img/Reasoning Rumble.jpg"
-
 
 export default [
     {
@@ -44,6 +46,39 @@ export default [
         contact: " Anshul Pratap Lakra - 6264653081"
     },
     {
-        img:img5
+        img: img5,
+        title: "Reasoning Rumble",
+        desc: "Reasoning Rumble invites teams of three to participate in dynamic debates, engaging in thought-provoking discussions on various topics. This event emphasizes critical thinking, persuasive argumentation, and effective communication, providing an excellent platform for participants to hone their debating skills. Whether seasoned debaters or newcomers, participants will experience a thrilling atmosphere filled with intellectual challenges and lively exchanges.",
+        date: "Round 1: Saturday (9th November) | Round 2: Sunday (10th November)",
+        fee: "₹100 per person",
+        price: "₹6000",
+        contact: "Divyansh Kumar Singh Chauhan - 7452916711"
     },
+    {
+        img: img8,
+        title: "Designathon",
+        desc: "Participants are challenged to design compelling ad campaign posters for a specific product, using a provided topic or concept. This event encourages creativity and artistic skill, as teams craft visually impactful designs that effectively promote the product. Designathon emphasizes spontaneity and innovative storytelling, giving participants the chance to express unique perspectives while conveying powerful messages through the art of ad campaign poster making.",
+        date: "Saturday (9th November) & Sunday (10th November)",
+        fee: "₹50 per person",
+        price: "₹7,500",
+        contact: "Hritik Routia - 6261430838"
+    },
+    {
+        img: img6,
+        title: "Cryptic Hunt",
+        desc: "Cryptic Hunt is an engaging online event where participants embark on a thrilling quest to solve intricate puzzles and riddles from home. Teams work collaboratively to decipher cryptic messages and tackle diverse challenges, enhancing creativity and problem-solving skills as they race towards victory. Whether skilled at riddles or strategic thinking, this event offers an exciting journey toward the ultimate prize.",
+        date: "8th November - 9th November",
+        fee: "Free",
+        price: "₹5000",
+        contact: "Priyadarshi Satyakam - 8109082272"
+    },
+    {
+        img: img7,
+        title: "Clash of Campus",
+        desc: "Clash of Campus is an exhilarating quiz competition inviting teams from various colleges to compete in a spirited test of knowledge, critical thinking, and teamwork. With multiple rounds covering diverse topics, participants will strategize and collaborate to answer challenging questions, making it the perfect event for trivia lovers and strategic minds.",
+        date: "9th November - 10th November",
+        fee: "₹50 per head",
+        price: "₹7500",
+        contact: "Kartik Pandey - 8009019862"
+    }
   ]
